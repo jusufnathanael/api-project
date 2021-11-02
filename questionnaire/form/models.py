6f51e26409243097a2c327a172c414021388977b
jusufnathanael <jusuf.n@gmail.com>
2021-11-02 13:22:59 +0800

    Final fix

--- questionnaire/form/models.py
+++ questionnaire/form/models.py
@@ -21,13 +21,3 @@ class Answer(models.Model):
     q3 = models.CharField(max_length=100)
     q4 = models.CharField(max_length=100)
     q5 = models.CharField(max_length=100)
-
-"""
-class Answer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    q1 = models.CharField(max_length=100)
-    q2 = models.CharField(max_length=100, choices=models.IntegerChoices('colour', 'blue red green yellow purple pink'))
-    q3 = models.CharField(max_length=100, choices=models.IntegerChoices('codelang', 'java ruby javascript golang'))
-    q4 = models.CharField(max_length=100, choices=models.IntegerChoices('lang', 'english chinese malay tamil hindi'))
-    q5 = models.CharField(max_length=100, choices=models.IntegerChoices('bool', 'yes no'))
-"""
